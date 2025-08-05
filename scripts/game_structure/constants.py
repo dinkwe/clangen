1a2a80a82a798c89959ec7068d292493592b41de
bingle <136284894+dinkwe@users.noreply.github.com>
2025-08-05 10:13:17 -0700

    Update constants.py

--- scripts/game_structure/constants.py
+++ scripts/game_structure/constants.py
@@ -1,6 +1,6 @@
 from pygame import Cursor, image, SYSTEM_CURSOR_ARROW
 import ujson
-import tomli
+#import tomli
 
 # this is just to make referencing main menu screens as a whole easier,
 # note that the clan creation screen is included and the clan settings screen is excluded. this is intended.
