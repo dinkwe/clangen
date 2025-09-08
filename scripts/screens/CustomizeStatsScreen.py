ac1c9b5c792b5f1f4d3b20dc63786ec47990c9d6
bingle <136284894+dinkwe@users.noreply.github.com>
2025-09-08 15:00:20 -0700

    more skills

--- scripts/screens/CustomizeStatsScreen.py
+++ scripts/screens/CustomizeStatsScreen.py
@@ -202,7 +202,7 @@ class CustomizeStatsScreen(Screens):
                        "COMFORTER", "CLEAN", "SONG", "TUNNELER", "ARTISAN", "EXPLORER", "CHEF", "DETECTIVE",
                        "BOOKMAKER", "ASSIST", "MEMORY", "AGILE", "DECORATOR", "WAKEFUL", "GARDENER", "PROPHET", "DREAM",
                        "DARK", "HEALER", "LORE", "KIT", "INSIGHTFUL", "MEDIATOR", "SWIMMER", "RUNNER", "HUNTER", "DAY",
-                       "NIGHT", "LUCK", "STRONG", "BUG", "BONES"]
+                       "NIGHT", "LUCK", "STRONG", "BUG", "BONES", "DANGER", "GEO", "OCCULT"]
         self.skill_strings_dict = {
             "TEACHER": SkillPath.TEACHER,
             "FIGHTER": SkillPath.FIGHTER,
@@ -284,7 +284,10 @@ class CustomizeStatsScreen(Screens):
             "LUCK": SkillPath.LUCK,
             "STRONG": SkillPath.STRONG,
             "BUG": SkillPath.BUG,
-            "BONES": SkillPath.BONES
+            "BONES": SkillPath.BONES,
+            "DANGER": SkillPath.DANGER,
+            "GEO": SkillPath.GEO,
+            "OCCULT": SkillPath.OCCULT
         }
         self.skills.sort()
         self.skills.insert(0, "None")
