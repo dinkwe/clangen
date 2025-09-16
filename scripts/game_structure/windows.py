781cd6917a8bb2e3f3a7234f162f2a311d1677c5
bingle <136284894+dinkwe@users.noreply.github.com>
2025-09-16 15:39:13 -0700

    EYEBALLS GALORE

--- scripts/game_structure/windows.py
+++ scripts/game_structure/windows.py
@@ -2613,8 +2613,9 @@ class CustomizeFilterWindow(UIWindow):
         self.customizer_screen = customizer_screen
 
         self.possible_tags = {
-            "eyes": ["all","red", "orange/brown", "yellow", "green", "blue", "purple", "black", "neos", "flutter", "lamp", "angel", "snail", "demon", "aquatic", "floral"],
-            "pelts": ["all", "ginger", "black", "white", "brown", "red", "orange", "yellow", "green", "blue", "purple", "black2"],
+            "eyes": ["all", "pink", "red", "orange", "brown", "yellow", "chartreuse", "green", "cyan", "light blue", "dark blue", "indigo", "violet", "black", "gray", "silver", "white",
+                     "angel", "aquatic", "demon", "floral", "flutter", "lamp", "neos", "snail"],
+            "pelts": ["all", "ginger", "black", "white", "brown", "red", "orange", "yellow", "green", "cyan", "blue", "purple", "black2"],
             "acc": ["all", "bones", "bugs", "stuff", "plants", "wild", "clothes", "bows", "collars", "feathers","disabilities", "animals","colorsplash", "games"],
             "white": ["all","little", "mid", "high", "mostly"],
             "tortie": ["all", "ginger", "black", "white", "brown", "red", "orange", "yellow", "green", "blue", "purple", "black2"]
